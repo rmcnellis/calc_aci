7ba0165e6e3c1abb9e5a8e3a00a37f5e59e605c5
[rmcnellis] <[risa.mcnellis@gmail.com]>
2022-04-05 08:53:34 -0400

    update formula for photosynthesis

--- johnson_aci.R
+++ johnson_aci.R
@@ -8,7 +8,7 @@ library(ggplot2)
 library(ggthemes)
 library(patchwork)
 library(minpack.lm)
-library (nlme)
+library(nlme)
 
 source("../johnsonberry_photosynthesis_R/johnson_model.R")
 
@@ -38,26 +38,20 @@ aci_c3 <- subset(aci_c3, !is.na(aci_c3$Photo))
 
 calc_gammastar = function(temp) {
   
-  rat = 100000 / 101325
-  
-  gammastar25 = 4.332 * rat  # Pa
+  gammastar25 = 4.332
   Hgm = 37830 # J mol-1
   R = 8.314        # J K-1 mol-1
-  O2 = 2.09476e5 # ppm
-  O2_0 = O2 * 1e-6 * 101325
-  O2_z = O2 * 1e-6 * 100000
-  
+
   temp_k = 273.15 + temp
   
-  gStar_pa = gammastar25 * exp((Hgm / R) * (1 / 298.15 - 1 / temp_k))
+  gStar = gammastar25 * exp((Hgm / R) * (1 / 298.15 - 1 / temp_k))
   
-  gStar_pa
+  gStar
   
 }
 
 nl <- 0.75
 nc <- 1
-gammas <- mean(calc_gammastar(aci_c3$Tleaf))
 Abs <- 0.85
 beta <- 0.52
 a2 <- Abs * beta
@@ -66,20 +60,39 @@ Kf <- 0.05e09
 Kd <- 0.55e09
 Kp1 <- 14.5e09
 phi1P_max <- Kp1 / (Kp1 + Kd + Kf)
-aci_c3$C <- aci_c3$Ci / 1e6
-Rd <- mean(aci_c3$Rd / 1e6)
-Q <- mean(aci_c3$Pari / 1e6)
-ci_trans <- 150 / 1e6 # estimated Ci transition point
+a1_phi1P_max <- a1 * phi1P_max
 
-A_function <- Photo ~ ((((vqmax * Q) / ((vqmax / (a1 * phi1P_max)) + Q)) / 
-                          (1 - (nl / nc) + (3 + 7 * (gammas / C)) / ((4 + 8 * gammas / C) * nc))) /
-                         (4 + 8 * gammas / C)) * (1 - gammas / C) - Rd
+# A_function <- Photo ~ ((((vqmax * Q) / ((vqmax / (a1 * phi1P_max)) + Q)) / 
+#                           (1 - (nl / nc) + (3 + 7 * (gammas / C)) / ((4 + 8 * gammas / C) * nc))) /
+#                          (4 + 8 * gammas / C)) * (1 - gammas / C) - Rd
+
+A_function <- Photo ~ (((vqmax * Q) / ((vqmax / a1_phi1P_max) + Q)) / 4) - Rd
+
+vqmax <- 360
+Q <- 1000 
+Rd <- 1
+Photo <- (((vqmax * Q) / ((vqmax / a1_phi1P_max) + Q)) / 4) - Rd
+Photo
 
 # fit the enzyme limited portion of the curve (Vcmax and Vpmax)
-aci_c3_c <- subset(aci_c3, C < ci_trans & C > 0)
-fit_function <- aci_c3_c %>% group_by(id) %>% 
-  group_map(~nlsLM(formula = A_function, data = .x, start = list(vqmax = 0.01), 
-                   control = nls.control(maxiter = 500, minFactor = 1/10000)), .keep = TRUE)
+id_list <- unique(aci_c3$id)
+i = 2
+for (i in 1:length(id_list)){
+  temp_data <- subset(aci_c3, id == id_list[i] & Ci > 375)
+  # temp_data$C <- temp_data$Ci / 1e6
+  temp_data$Rd <- mean(temp_data$Rd)
+  temp_data$Q <- mean(temp_data$Pari)
+  gammas <- mean(calc_gammastar(temp_data$Tleaf))
+  temp_nls <- nlsLM(A_function, data = temp_data,
+                    start = list(vqmax = 0.01),
+                    control = nls.control(maxiter = 500, minFactor = 1 / 10000))
+  summary(temp_nls)
+  
+}
+ggplot(subset(aci_c3, aci_c3$id == id_list[2]), aes(x = Ci, y = Photo)) + geom_point()
+# fit_function <- aci_c3_c %>% group_by(id) %>% 
+#   group_map(~nlsLM(formula = A_function, data = .x, start = list(vqmax = 0.01), 
+#                    control = nls.control(maxiter = 500, minFactor = 1/10000)), .keep = TRUE)
 # fit_function <- nlsLM(A_function, data = aci_c3_c,
 #                       start = list(vqmax = 0.01),
 #                       control = nls.control(maxiter = 500, minFactor = 1 / 10000))
