c521ee25d814fe1ae2e77c0d466d9518b6e42596
[rmcnellis] <[risa.mcnellis@gmail.com]>
2022-04-05 20:48:08 -0400

    add test script

---
+++ test_calc_aci.R
@@ -0,0 +1,13 @@
+# Test A/Ci curve fitting
+
+test <- data.frame("Photo" = c(0.5, 8, 14, 20, 25, 27, 28, 30, 31, 31.5, 32, 32.8, 33, 33.8, 34, 34.6, 35, 35.3, 35.5, 36), 
+                   "Ci" = seq(50, 1000, 50), 
+                   "Rd" = rep(1, 20),
+                   "PAR" = rep(1000, 20),
+                   "Tleaf" = rep(25, 20))
+
+ggplot(test, aes(x = Ci, y = Photo)) + geom_point()
+
+
+calc_vmax(data = subset(aci_c3, id == "Auburn_Iopa_1"), ci_trans = 375)
+
